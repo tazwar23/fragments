e32bef53f229f0cdd8f72fcbd29ef17579cac54f
tazwar23 <atazwar@myseneca.ca>
2023-05-29 23:18:06 -0400

    Created health.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,17 @@
+// .eslintrc.js
+
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-    }
-}
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+    // Add this next line to configure ESLint for Jest, see:
+    // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+    jest: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  rules: {},
+};
