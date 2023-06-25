045a71de49dc44958cfdfdf45ecb48a881441ee3
tazwar23 <atazwar@myseneca.ca>
2023-06-25 19:09:26 -0400

    Added try catch blocks for GET and POST

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -5,22 +5,30 @@
  */
 const frag = require('../../model/fragment');
 
-const { createSuccessResponse } = require('../../response');
+const { createSuccessResponse, createErrorResponse } = require('../../response');
 module.exports.get = (req, res) => {
   // TODO: this is just a placeholder to get something working...
-  var expand = req.query.expand == 1 ? true : false;
+  try {
+    var expand = req.query.expand == 1 ? true : false;
 
-  frag.Fragment.byUser(req.user, expand).then((data) => {
-    var resData = { fragments: data };
-    res.status(200).json(createSuccessResponse(resData));
-  });
+    frag.Fragment.byUser(req.user, expand).then((data) => {
+      var resData = { fragments: data };
+      res.status(200).json(createSuccessResponse(resData));
+    });
+  } catch (msg) {
+    res.status(400).json(createErrorResponse(msg));
+  }
 };
 
 module.exports.getOne = (req, res) => {
-  var fragObj = new frag.Fragment({ id: req.params.id, ownerId: req.user, type: 'text/plain' });
-  fragObj.getData().then((data) => {
-    const buffObject = Buffer.from(data);
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(200).send(buffObject.toString());
-  });
+  try {
+    var fragObj = new frag.Fragment({ id: req.params.id, ownerId: req.user, type: 'text/plain' });
+    fragObj.getData().then((data) => {
+      const buffObject = Buffer.from(data);
+      res.setHeader('Content-Type', 'text/plain');
+      res.status(200).send(buffObject.toString());
+    });
+  } catch (msg) {
+    res.status(404).json(createErrorResponse(404, msg));
+  }
 };
--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -5,18 +5,22 @@ module.exports = async (req, res) => {
   // TODO: Implement the logic for creating a fragment based on the request body
 
   // // Assuming the request body contains necessary data for creating a fragment
-  const fragmentData = req.body;
+  try {
+    const fragmentData = req.body;
 
-  var fragObj = new frag.Fragment({ ownerId: req.user, type: req.headers['content-type'] });
+    var fragObj = new frag.Fragment({ ownerId: req.user, type: req.headers['content-type'] });
 
-  fragObj.save();
-  await fragObj.setData(fragmentData);
+    fragObj.save();
+    await fragObj.setData(fragmentData);
 
-  const fragmentURL = `http://${req.headers.host}/v1/fragments/${fragObj.id}`;
+    const fragmentURL = `http://${req.headers.host}/v1/fragments/${fragObj.id}`;
 
-  // Set the Location header with the fragment URL
-  res.setHeader('Location', fragmentURL);
-  var fragment = { fragment: fragObj };
+    // Set the Location header with the fragment URL
+    res.setHeader('Location', fragmentURL);
+    var fragment = { fragment: fragObj };
 
-  res.status(201).json(createSuccessResponse(fragment));
+    res.status(201).json(createSuccessResponse(fragment));
+  } catch (msg) {
+    res.status(404).json(createErrorResponse(404, msg));
+  }
 };
