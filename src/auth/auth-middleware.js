56cea13aad46986e39c0daf7b9a048f48f673956
tazwar23 <atazwar@myseneca.ca>
2023-06-25 12:48:13 -0400

    A1 routing done

--- src/auth/auth-middleware.js
+++ src/auth/auth-middleware.js
@@ -1,15 +1,43 @@
-const hash = require('../../src/hash');
+const passport = require('passport');
 
-describe('hash()', () => {
-  const email = 'user1@example.com';
+const { createErrorResponse } = require('../response');
+const hash = require('../hash');
+const logger = require('../logger');
 
-  test('email addresses should get hashed using sha256 to hex strings', () => {
-    const hashedEmail = 'b36a83701f1c3191e19722d6f90274bc1b5501fe69ebf33313e440fe4b0fe210';
-    expect(hash(email)).toEqual(hashedEmail);
-  });
+/**
+ * @param {'bearer' | 'http'} strategyName - the passport strategy to use
+ * @returns {Function} - the middleware function to use for authentication
+ */
+module.exports = (strategyName) => {
+  return function (req, res, next) {
+    /**
+     * Define a custom callback to run after the user has been authenticated
+     * where we can modify the way that errors are handled, and hash emails.
+     * @param {Error} err - an error object
+     * @param {string} email - an authenticated user's email address
+     */
+    function callback(err, email) {
+      // Something failed, let the the error handling middleware deal with it
+      if (err) {
+        logger.warn({ err }, 'error authenticating user');
+        return next(createErrorResponse(500, 'Unable to authenticate user'));
+      }
 
-  test('hashing should always return the same value for a given string', () => {
-    const email = 'user1@example.com';
-    expect(hash(email)).toEqual(hash(email));
-  });
-});
+      // Not authorized, return a 401
+      if (!email) {
+        return res.status(401).json(createErrorResponse(401, 'Unauthorized'));
+      }
+
+      // Authorized. Hash the user's email, attach to the request, and continue
+      req.user = hash(email);
+      logger.debug({ email, hash: req.user }, 'Authenticated user');
+
+      // Call the next function in the middleware chain (e.g. your route handler)
+      next();
+    }
+
+    // Call the given passport strategy's authenticate() method, passing the
+    // req, res, next objects.  Invoke our custom callback when done.
+    passport.authenticate(strategyName, { session: false }, callback)(req, res, next);
+  };
+};
