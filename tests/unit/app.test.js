bddb849798318a4f6b93b6a5e701809d1687e9bb
tazwar23 <atazwar@myseneca.ca>
2023-06-28 14:21:19 -0400

    A1 done

--- tests/unit/app.test.js
+++ tests/unit/app.test.js
@@ -3,5 +3,5 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 describe('Test for app', () => {
-  test('Test for invalid request', () => request(app).get('/InvalidRoute').expect(404));
+  test('Test for invalid request ', () => request(app).get('/InvalidRoute').expect(404));
 });
