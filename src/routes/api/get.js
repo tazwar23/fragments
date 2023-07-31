c1a637bc0a6d68990265beb053535f273bf580d6
tazwar23 <atazwar@myseneca.ca>
2023-07-30 20:22:36 -0400

    Fixed promise issues with both getOne,getOneWithInfo route

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -22,10 +22,24 @@ module.exports.get = (req, res) => {
 };
 
 module.exports.getOne = async (req, res) => {
-  var fragObj = await frag.Fragment.byId(req.user, req.params.id);
-
-  fragObj
-    .getData()
+  // try {
+  //   let fragObject = await frag.Fragment.byId(req.user, req.params.id);
+  //   fragObject.getData().then((data) => {
+  //     if (data) {
+  //       var buffObject = Buffer.from(data);
+  //       buffObject = buffObject.toString();
+  //       res.setHeader('Content-Type', 'text/plain');
+  //       logger.debug({ buffObject }, 'Got fragments data from V1/fragments/:id');
+  //       res.status(200).send(buffObject);
+  //     } else {
+  //       throw new Error('Object not found');
+  //     }
+  //   });
+  // } catch (error) {
+  //   res.status(404).json(createErrorResponse(404, error.message));
+  // }
+  frag.Fragment.byId(req.user, req.params.id)
+    .then((fragObj) => fragObj.getData())
     .then((data) => {
       if (data) {
         var buffObject = Buffer.from(data);
@@ -42,11 +56,17 @@ module.exports.getOne = async (req, res) => {
     });
 };
 
-module.exports.getOneWithInfo = (req, res) => {
+module.exports.getOneWithInfo = async (req, res) => {
+  // let fragObject = await frag.Fragment.byId(req.user, req.params.id);
+
+  // try {
+  // } catch (error) {
+  //   res.status(404).json(createErrorResponse(404, error.message));
+  // }
   frag.Fragment.byId(req.user, req.params.id)
-    .then((data) => {
-      if (data) {
-        res.status(200).send(createSuccessResponse({ fragment: data }));
+    .then((fragObject) => {
+      if (fragObject) {
+        res.status(200).send(createSuccessResponse({ fragment: fragObject }));
       } else {
         throw new Error('Object not found');
       }
