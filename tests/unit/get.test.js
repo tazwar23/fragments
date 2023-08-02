c051b83c18ba3c1f6e3c9befadd75e5ac217f71f
tazwar23 <atazwar@myseneca.ca>
2023-08-01 22:58:43 -0400

    Added tests to get route

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -45,4 +45,42 @@ describe('GET /v1/fragments', () => {
     const res = await request(app).get('/v1/fragments/s53245').auth('user1@email.com', 'password1');
     expect(res.statusCode).toBe(404);
   });
+  test('Expect get one to convert markdown fragments to .html', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .set('Content-Type', 'text/markdown')
+      .auth('user1@email.com', 'password1')
+      .send('This is a fragment');
+
+    const location = res.headers['location'];
+    const v1Index = location.indexOf('/v1');
+    const v1Part = location.substring(v1Index);
+    const getRes = await request(app).get(`${v1Part}.html`).auth('user1@email.com', 'password1');
+    expect(getRes.text).toBe('<p>This is a fragment</p>\n');
+  });
+  test('Expect getOne/info to give correct metadata for specified id', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .set('Content-Type', 'text/plain')
+      .auth('user1@email.com', 'password1')
+      .send('This is a fragment');
+
+    const location = res.headers['location'];
+    const v1Index = location.indexOf('/v1');
+    const v1Part = location.substring(v1Index);
+
+    const getAllRes = await request(app)
+      .get('/v1/fragments?expand=1')
+      .auth('user1@email.com', 'password1');
+
+    const getOneRes = await request(app).get(`${v1Part}/info`).auth('user1@email.com', 'password1');
+    //Use the third object from getAll because two other objects have been created in the previous tests.
+    expect(getAllRes.body.fragments[2]).toEqual(getOneRes.body.fragment);
+  });
+  test('Expect getOne/info to give 404 if url isnt correct ', async () => {
+    const res = await request(app)
+      .get('/v1/fragments/s53245/info')
+      .auth('user1@email.com', 'password1');
+    expect(res.statusCode).toBe(404);
+  });
 });
